348078205cc0795bc033b89a5acbcd40ed741db2
Zack Scholl <zack.scholl@gmail.com>
2020-11-24 17:06:04 -0800

    set volume

--- norns.online.lua
+++ norns.online.lua
@@ -99,7 +99,7 @@ function init()
     redraw()
   end)
 
-  params:add_taper("roomvolume","room volume",0,100,1,0,"")
+  params:add_taper("roomvolume","room volume",0,100,10,100,"")
   params:set_action("roomvolume",function(x)
     if settings.allowroom and settings.room ~= "" then 
       for i=0,4 do
